616290e1b527954442d4796431a0e0b3a2b0f171
lwojcic1 <01169261@pw.edu.pl>
2023-04-15 17:12:19 +0200

    working admin site with models

--- warehouse40/magazine/admin.py
+++ warehouse40/magazine/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product, WarehouseZone, Worker, Order, ProductOrder
 
-# Register your models here.
+
+admin.site.register(Product)
+admin.site.register(WarehouseZone)
+admin.site.register(Worker)
+admin.site.register(Order)
+admin.site.register(ProductOrder)
