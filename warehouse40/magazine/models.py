d8e1e14b88880ce807b30057601148c1141c7bd6
Paweł Dombrzalski <pawel.dombrzalski@gmail.com>
2023-04-15 14:40:06 +0200

    Added some orm objects

--- warehouse40/magazine/models.py
+++ warehouse40/magazine/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from enum import Enum
 
 
+class WarehouseZone(models.Model):
+    name = models.CharField(max_length=255)
+
+
 class Product(models.Model):
     class LoadPriority(Enum):
         A = "AA"
@@ -13,8 +17,23 @@ class Product(models.Model):
     quantity = models.IntegerField()
     price = models.FloatField()
     zone_id = models.ForeignKey(
-        "app.Model", verbose_name=_(""), on_delete=models.CASCADE
+        WarehouseZone, verbose_name=_(""), on_delete=models.CASCADE
     )
     load_priority = models.CharField(
         max_length=2, choices=LoadPriority, default=LoadPriority.A
     )
+
+
+class Worker(models.Model):
+    name = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=255)
+
+
+class Order(models.Model):
+    buyer = models.CharField(max_length=255)
+    date_of_order = models.DateField(null=True)
+    estimated_time = models.TimeField(null=True)
+
+
+# class ProductOrder(models.Model):
