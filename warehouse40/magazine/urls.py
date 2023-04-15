9b24be6480c8567c6f83f034d0f88fa6004a987b
Piotras laptop <Piotr.kow.59@gmail.com>
2023-04-15 18:06:13 +0200

    urls

--- warehouse40/magazine/urls.py
+++ warehouse40/magazine/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
-from .views import product_list
+from .views import product_list, load_data, index
 
-<<<<<<< HEAD
-urlpatterns = [path("", views.index, name="index"),
-               path('load_data/', views.load_data, name="load_data")]
-=======
-urlpatterns = [
-    path('', product_list, name='product_list'),
-    # add other URL patterns as needed
-]
->>>>>>> f52e9bda3b01bf35ce30d479a2059e4b63fa1d20
+urlpatterns = [path("", index, name="index"),
+               path('load_data/', load_data, name="load_data"),
+               path('', product_list, name='product_list'),]
